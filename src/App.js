f98521afd9a39f8a702446368010f9c3fa574357
Dan Fellner <dfellner92@gmail.com>
2021-09-20 14:11:21 -0400

    trying

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Hopefully this works</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
